a5f7723da581131f63fdb85979e150dcbf78ee14
koni24 <konisha.24.kar@gmail.com>
2023-01-29 22:24:35 +0530

    navbar_footer

--- amazon-clone/src/Components/NavBar/Navigation.js
+++ amazon-clone/src/Components/NavBar/Navigation.js
@@ -9,46 +9,57 @@ class NavBar extends Component {
     return (
       <div>
         <div className="navbar_component">
-        <div className="navbar_logo"></div>
-        <div className="navbar_locator">
-          <div className="navbar_locatorImage"></div>
-          <div className="navbar_location">Bangalore</div>
-        </div>
-        <div className="navbar_searchcomponent">
-          <div>
-            <select className="nav_dropdown">
-              <option value="All">All</option>
-              <option value="Alexa">Alexa</option>
-              <option value="Books">Books</option>
-              <option value="Baby">Baby</option>
-              <option value="Beauty">Beauty</option>
-              <option value="Clothes">Clothes</option>
-            </select>
+          <div className="navbar_logo"></div>
+          <div className="navbar_locator">
+            <div className="navbar_locatorImage"></div>
+            <div className="navbar_location">Bangalore</div>
           </div>
-          <div>
-            <input type="text" className="navbar_searchbox" />
+          <div className="navbar_searchcomponent">
+            <div>
+              <select className="nav_dropdown">
+                <option value="All">All</option>
+                <option value="Alexa">Alexa</option>
+                <option value="Books">Books</option>
+                <option value="Baby">Baby</option>
+                <option value="Beauty">Beauty</option>
+                <option value="Clothes">Clothes</option>
+              </select>
+            </div>
+            <div>
+              <input type="text" className="navbar_searchbox" />
+            </div>
+            <div className="navbar_searchboxdiv">
+              <div className="navbar_searchicon"></div>
+            </div>
           </div>
-          <div className="navbar_searchboxdiv">
-            <div className="navbar_searchicon"></div>
+          <div className="navbar_text navbar_signin">
+            <div style={{ fontSize: "14px" }}>Hello, Sign In</div>
+            <div style={{ fontWeight: "bold" }}>Account & List</div>
           </div>
-        </div>
-        <div className="navbar_text navbar_signin">
-          <div style={{ fontSize: "14px" }}>Hello, Sign In</div>
-          <div style={{ fontWeight: "bold" }}>Account & List</div>
-        </div>
-        <div className="navbar_text navbar_returns">
-          <div style={{ fontSize: "14px" }}>Returns</div>
-          <div style={{ fontWeight: "bold" }}> & Order</div>
-        </div>
-        {/* <Link to="/checkout"> */}
+          <div className="navbar_text navbar_returns">
+            <div style={{ fontSize: "14px" }}>Returns</div>
+            <div style={{ fontWeight: "bold" }}> & Order</div>
+          </div>
+          {/* <Link to="/checkout"> */}
           <div className="navbar_text navbar_cart">
             <div src="" className="cart_image"></div>
             <div className="cart_item">0</div>
             <div className="navbar_text_cart">Cart</div>
           </div>
-        {/* </Link> */}
-      </div>
-      <div></div>
+          {/* </Link> */}
+        </div>
+        <div className="navbar_footer">
+          <div className="navbar_footer_text">Best Seller</div>
+          <div className="navbar_footer_text">Mobile</div>
+          <div className="navbar_footer_text">Amazon Pay</div>
+          <div className="navbar_footer_text">Fashion</div>
+          <div className="navbar_footer_text">Electronics</div>
+          <div className="navbar_footer_text">Prime</div>
+          <div className="navbar_footer_text">New Release</div>
+          <div className="navbar_footer_text">Customer Service</div>
+          <div className="navbar_footer_text">Computers</div>
+          <div className="navbar_footer_text">Home & Kitchen</div>
+        </div>
       </div>
     );
   }
